e194f1e30a740ec58091d08068f522b1ff771386
HexaField <joshfield999@gmail.com>
2024-08-29 14:10:28 +1000

    remove public path

--- src/functions/botHookFunctions.ts
+++ src/functions/botHookFunctions.ts
@@ -95,7 +95,6 @@ export function serializeEngine() {
   const engine = {
     userId: Engine.instance.userID,
     store: Engine.instance.store,
-    publicPath: Engine.instance.store.publicPath,
     xrFrame: getState(XRState).xrFrame,
     isEditor: getState(EngineState).isEditor
   }
